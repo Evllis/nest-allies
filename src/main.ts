1d9144205f27b1d8d7ad9b175845963d8194000e
Evllis <7284395@qq.com>
2022-01-18 18:00:02 +0800

    🆕 introduce_features(middleware): 对入参设置logger拦截器, 保存日志文件

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+import * as express from 'express'
+
+import { logger } from '/@/middleware/logger.middleware'
+
 export const IS_DEV = process.env.NODE_ENV !== 'production'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    app.use(express.json()) // For parsing application/json
+    app.use(express.urlencoded({ extended: true })) // For parsing application/x-www-form-urlencoded
+    app.use(logger)
     // 全局路由前缀
     app.setGlobalPrefix('nest-allies')
     await app.listen(4000)
